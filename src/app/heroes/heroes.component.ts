2d2c43cf2da072d7f52c396b63bac6a3c805326d
monsterofhell <morris007chris1@gmail.com>
2020-07-21 18:37:26 +0530

    2 files modified

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,9 +12,10 @@ export class HeroesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
+  };
+  heroes =HEROES;
   selectedHero: Hero;
-  
+
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
   }
