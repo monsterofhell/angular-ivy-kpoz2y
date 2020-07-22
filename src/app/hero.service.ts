3a30c088ecf71f74a28a027934db9af4007510d5
monsterofhell <morris007chris1@gmail.com>
2020-07-22 21:04:43 +0530

    4 files modified, 3 added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES} from './mock-heroes';
-
+import {Observable, of } from 'rxjs';
+//Registering the provider in the @Injectable metadata also allows Angular to //optimize an app by removing the service if it turns out not to be used after all.
 @Injectable(
   //This is registering this service to the roort provider, whichever class 
   //needs this would be provided by angular automatically if correctly defined.
@@ -10,8 +11,8 @@ import { HEROES} from './mock-heroes';
 export class HeroService {
 
   constructor() { }
-getHeroes(): Hero[] {
-  return HEROES;
-}
+  getHeroes(): Observable<Hero[]> {
+     return of(HEROES);
+  }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { Component, OnInit } from "@angular/core";
+import { HeroService } from "../hero.service";
+import { Hero } from "../hero";
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: "app-heroes",
+  templateUrl: "./heroes.component.html",
+  styleUrls: ["./heroes.component.css"]
 })
 export class HeroesComponent implements OnInit {
-
-  constructor() { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
-  };
-  heroes =HEROES;
+    this.getHeroes();
+  }
+  heroes: Hero[];
   selectedHero: Hero;
-
-  onSelect(hero: Hero): void{
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes));
+  }
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-
 }
