50d1043777db77124f6687181849649a412291e1
monsterofhell <morris007chris1@gmail.com>
2020-07-23 08:50:21 +0530

    4 files modified, 1 added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES} from './mock-heroes';
 import {Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 //Registering the provider in the @Injectable metadata also allows Angular to //optimize an app by removing the service if it turns out not to be used after all.
 @Injectable(
   //This is registering this service to the roort provider, whichever class 
@@ -10,9 +11,10 @@ import {Observable, of } from 'rxjs';
 )
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
   getHeroes(): Observable<Hero[]> {
-     return of(HEROES);
+    this.messageService.add('HerosService: fetched heroes');
+    return of(HEROES);
   }
 
 }
